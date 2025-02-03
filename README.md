85cbf34e8acd30cf5b32dc3e1913ab4de869f5dc
Mohitkaila <mohit.kaila@ucalgary.ca>
2025-02-02 19:50:30 -0700

    Landing Page done

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Recipe-Buddy
+
+For Frontend :
+
+cd frontend
+npm install
+npm start  
+
+For Backend : 
+
+cd backend
+pip install -r requirements.txt
+python app.py  
+
+
---
+++ frontend/src/Landingpage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Landingpage.css";
+
+const Landingpage = () => {
+  return (
+    <div className="landing-container">
+      <h1 className="landing-title">Recipe Buddy</h1>
+      <p className="landing-subtitle">
+        Don't Let It Rot
+      </p>
+      <button className="landing-button">Get Started</button>
+    </div>
+  );
+};
+
+export default Landingpage;
