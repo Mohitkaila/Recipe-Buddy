69efc52d3a79cb2037332892745188abb5e4dc38
Mohitkaila <mohit.kaila@ucalgary.ca>
2025-03-18 03:51:46 -0600

    removed test.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
-const router = express.Router(); 
+const router = express.Router();  
 
 router.post("/register", async (req, res) => {
   try {
